76699e6d03d9d726f2b2b3cf2f3b9cccea607c45
RonaldColyar <PhantexCreations@gmail.com>
2021-10-07 00:31:14 -0500

    relations

--- relation_manager.py
+++ relation_manager.py
@@ -1,15 +1,20 @@
 import json
 
-
 class RelationManager:
     def __init__(self):
-        self.relations = self.get_relation()
+        self.relations = self.get_relations()
 
     def check_passive_data_for_matching_conditions_and_execute_actions(self,data):
         pass
 
     def get_relations(self):
-        pass
+        relations = []
+        file = open("relations.json","r") 
+        file_data = file.read()
+        relation_data =  json.loads(file_data)
+        for relation in relation_data["relations"]:
+            relations.append(relation)
+        return relation
 
     def condition_present_in_passive_data(self,key,value,data,bot_name):
         if bot_name in data:
