b33762807db4b47f3d8832a7e5d768d441fc9c3b
RonaldColyar <PhantexCreations@gmail.com>
2021-11-25 05:32:26 -0600

    changed relation comparing

--- TestClients/test_non_bot.py
+++ TestClients/test_non_bot.py
@@ -24,10 +24,10 @@ class AsyncTests(unittest.IsolatedAsyncioTestCase):
         
     async def test(self):
         relation = {
-            "device_name":"water_valve", 
-            "action":"open", 
+            "device_name":"test_bot_execute", 
+            "action":"test_trigger", 
             "conditions":[
-                {"device_name":"test_bot_execute", 
+                {"device_name":"test_bot_read", 
                 "test_field":True}]}
         external_monitor_websocket = await self.connect_to_external_monitor()
         general_server_websocket = await self.connect_to_general_server()
@@ -37,20 +37,7 @@ class AsyncTests(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(response,"success")
         response_dict = await self.execute_basic_request_reponse(websocket,"view_relations")
         self.assertEqual(response_dict["type"],"current_relations")
-        self.assertEqual(len(response_dict["relations"]),1)
-        gathered_relation = response_dict["relations"][0]
-        
-        #compare outer relation fields directly
-        self.assertEqual(gathered_relation["device_name"],relation["device_name"])
-        self.assertEqual(gathered_relation["action"],relation["action"])
-        self.assertEqual(len(gathered_relation["conditions"]),1)
-
-        #compare inner condition
-        gathered_conditions = gathered_relation["conditions"]
-        self.assertEqual(
-            gathered_conditions[0]["device_name"],relation["conditions"][0]["device_name"])
-        self.assertEqual(
-            gathered_conditions[0]["test_field"],relation["conditions"][0]["test_field"])
+        self.compare_relations(relation,response_dict["relation"])
         
     async def remove_relation(self,websocket,relation):
         response = await self.add_or_remove_relation(websocket,relation,"remove_relation")
@@ -71,6 +58,27 @@ class AsyncTests(unittest.IsolatedAsyncioTestCase):
         response_dict = json.loads(response)
         self.assertEqual(response_dict["status"],"success")
         return response_dict
+
+    async def compare_relations(self,original_relation,gathered_relation):
+        original_conditions = original_relation["conditions"]
+        gathered_conditions = gathered_relation["conditions"]
+        self.assertEqual(original_relation["device_name"], gathered_relation["device_name"])
+        self.assertEqual(original_relation["action"], gathered_relation["action"])
+        self.assertEqual(len(original_conditions),len(gathered_conditions))
+
+        """
+        The conditions gathered should reside at the same index as 
+        the original one sent, since the service doesn't change the 
+        order of the indexes in memory.
+        """
+        for i in range(len(original_conditions)):
+            current_original_condition = original_conditions[i]
+            current_gathered_condition = gathered_conditions[i]
+
+            for key in current_original_condition.keys():
+                original_value = current_original_condition[key]
+                gathered_value = current_gathered_condition[key]
+                self.assertEqual(original_value,gathered_value)
 
     async def add_or_remove_relation(self,websocket,relation,op_code):
         request = {"request":op_code,"password":"", "relation":relation}
