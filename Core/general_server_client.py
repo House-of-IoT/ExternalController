163402a12e3db0baa4b3eb71d26d309f597b85fd
RonaldColyar <phantexcreations@gmail.com>
2021-11-27 01:41:12 -0600

    minor

--- Core/general_server_client.py
+++ Core/general_server_client.py
@@ -28,13 +28,14 @@ class GeneralServerClient:
                 else:
                     print("authentication failed")
             except Exception as e:
-                traceback.print_exc()
-            
+                traceback.print_exc()       
 
     async def establish_websocket_connection(self):
         self.websocket = await self.client.establish_connection()
 
     async def gather_data_and_analyze(self):
+        self.parent.console_logger.log_generic_row(
+            "Successfully connected to the GeneralServer with authetication","green")
         while True:
             await asyncio.sleep(5)
             try:
